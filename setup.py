26b00a112404952abbd32d96b9b4e69102a17299
Victor Chahuneau <vchahune@cs.cmu.edu>
2012-05-30 01:36:02 -0400

    Make python build fully independent

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+lbfgs_sources = ['liblbfgs/lbfgs.c']
+creg_sources = ('fdict.cc', 'dict.cc', 'gzstream.cc', 'filelib.cc', 'json_feature_map_lexer.cc')
+creg_sources = ['creg/'+fn for fn in creg_sources]
+
 ext_modules = [
     Extension(name='creg',
-        sources=['python/creg.pyx', 'creg/json_feature_map_lexer.cc'],
+        sources=['python/creg.pyx'] + lbfgs_sources + creg_sources,
         language='C++', 
-        include_dirs=['.'],
-        extra_objects = ['dist/lib/libcregutils.a', 'dist/lib/liblbfgs.a'],
+        include_dirs=['.', 'creg'],
         libraries=['boost_program_options-mt'])
 ]
 
